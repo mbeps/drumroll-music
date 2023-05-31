c3cd43987d8386a6c1e1e5d82541784354e60f4c
Maruf <bepary71@gmail.com>
2023-05-31 17:40:46 +0100

    Created header skeleton - Displays back and forward buttons on desktop - Displays home and search buttons on mobile - Always displays authentication buttons

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,4 +1,5 @@
 import Header from "@/components/Header";
+import ListItem from "@/components/ListItem";
 import Image from "next/image";
 
 export default function Home() {
@@ -13,7 +14,37 @@ export default function Home() {
         overflow-y-auto
       "
     >
-      <Header>Header</Header>
+      <Header>
+        {" "}
+        <div className="mb-2">
+          <h1
+            className="
+            text-white 
+              text-3xl 
+              font-semibold
+            "
+          >
+            Welcome back
+          </h1>
+          <div
+            className="
+              grid 
+              grid-cols-1 
+              sm:grid-cols-2 
+              xl:grid-cols-3 
+              2xl:grid-cols-4 
+              gap-3 
+              mt-4
+            "
+          >
+            <ListItem
+              name="Liked Songs"
+              image="/images/liked.png"
+              href="liked"
+            />
+          </div>
+        </div>
+      </Header>
     </div>
   );
 }
---
+++ components/Header.tsx
@@ -0,0 +1,127 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import React from "react";
+import { twMerge } from "tailwind-merge";
+import { RxCaretLeft, RxCaretRight } from "react-icons/rx";
+import { HiHome } from "react-icons/hi";
+import { BiSearch } from "react-icons/bi";
+import { FaUserAlt } from "react-icons/fa";
+import Button from "./Button";
+
+interface HeaderProps {
+  children: React.ReactNode;
+  className?: string;
+}
+
+const Header: React.FC<HeaderProps> = ({ children, className }) => {
+  const router = useRouter();
+
+  const handleLogout = () => {
+    // TODO: handle logging out
+    console.log("Log Out");
+  };
+
+  return (
+    <div
+      className={twMerge(
+        `
+				h-fit 
+				bg-gradient-to-b 
+				from-rose-900
+				p-6
+				`,
+        className
+      )}
+    >
+      <div className="w-full mb-4 flex items-center justify-between">
+        {/* Desktop View */}
+        <div className="hidden md:flex gap-x-2 items-center">
+          <button
+            onClick={() => router.back()}
+            className="
+              rounded-lg 
+              bg-black 
+              flex 
+              items-center 
+              justify-center 
+              cursor-pointer 
+              hover:opacity-75 
+              transition
+            "
+          >
+            <RxCaretLeft className="text-white" size={35} />
+          </button>
+          <button
+            onClick={() => router.forward()}
+            className="
+              rounded-lg 
+              bg-black 
+              flex 
+              items-center 
+              justify-center 
+              cursor-pointer 
+              hover:opacity-75 
+              transition
+            "
+          >
+            <RxCaretRight className="text-white" size={35} />
+          </button>
+        </div>
+        {/* Mobile View */}
+        <div className="flex md:hidden gap-x-2 items-center">
+          <button
+            onClick={() => router.push("/")}
+            className="
+              rounded-lg 
+              p-2 
+              bg-black 
+              flex 
+              items-center 
+              justify-center 
+              cursor-pointer 
+              hover:opacity-75 
+              transition
+            "
+          >
+            <HiHome className="text-white" size={20} />
+          </button>
+          <button
+            onClick={() => router.push("/search")}
+            className="
+              rounded-lg
+              p-2 
+              bg-black 
+              flex 
+              items-center 
+              justify-center 
+              cursor-pointer 
+              hover:opacity-75 
+              transition
+            "
+          >
+            <BiSearch className="text-white" size={20} />
+          </button>
+        </div>
+        {/*  */}
+        <div className="flex justify-between items-center gap-x-4">
+          <>
+            <div className="flex gap-x-4 items-center">
+              <Button onClick={handleLogout} className="bg-black px-6 py-2">
+                Logout
+              </Button>
+              <Button
+                onClick={() => router.push("/account")}
+                className="bg-black"
+              >
+                <FaUserAlt />
+              </Button>
+            </div>
+          </>
+        </div>
+      </div>
+      {children}
+    </div>
+  );
+};
+export default Header;
