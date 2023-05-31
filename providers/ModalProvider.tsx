89d1859cf3d3543fc4e9b011ca43d8c329ce10ad
Maruf <bepary71@gmail.com>
2023-05-31 20:44:51 +0100

    Created base modal component - Other modals are based from this - Redix UI was used - Created modal provider to make it accessible throughout the app

---
+++ providers/ModalProvider.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { ProductWithPrice } from "@/types/types";
+import AuthModal from "@/components/Modals/AuthModal";
+
+interface ModalProviderProps {
+  products: ProductWithPrice[];
+}
+
+const ModalProvider: React.FC<ModalProviderProps> = ({ products }) => {
+  const [isMounted, setIsMounted] = useState(false);
+
+  /**
+   * Prevents errors in server side rendering.
+   * Modal can cause hydration errors.
+   * Do not render modal if rendering on the server.
+   */
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) {
+    return null;
+  }
+
+  return <></>;
+};
+
+export default ModalProvider;
