7ef966b88a607d12fd5ca56cfb1f372099589cfa
Maruf Bepary <bepary71@gmail.com>
2023-06-02 18:28:35 +0100

    Added ability to play and pause an already playing song on the player

--- components/Player/PlayerContent.tsx
+++ components/Player/PlayerContent.tsx
@@ -1,8 +1,10 @@
 "use client";
 
 import { useEffect, useState } from "react";
+import { BsPauseFill, BsPlayFill } from "react-icons/bs";
 import useSound from "use-sound";
 
+import usePlayer from "@/hooks/usePlayer";
 import { Song } from "@/types/types";
 import LikeButton from "../LinkButton";
 import MediaItem from "../MediaItem";
@@ -13,8 +15,11 @@ interface PlayerContentProps {
 }
 
 const PlayerContent: React.FC<PlayerContentProps> = ({ song, songUrl }) => {
+  const player = usePlayer();
   const [volume, setVolume] = useState(1);
   const [isPlaying, setIsPlaying] = useState(false);
+
+  const PlayPauseIcon = isPlaying ? BsPauseFill : BsPlayFill;
 
   // player
   const [play, { pause, sound }] = useSound(songUrl, {
@@ -36,12 +41,82 @@ const PlayerContent: React.FC<PlayerContentProps> = ({ song, songUrl }) => {
     };
   }, [sound]);
 
+  const handlePlay = () => {
+    if (!isPlaying) {
+      play();
+    } else {
+      pause();
+    }
+  };
+
   return (
     <div className="grid grid-cols-2 md:grid-cols-3 h-full">
       <div className="flex w-full justify-start">
         <div className="flex items-center gap-x-4">
           <MediaItem song={song} />
           <LikeButton songId={song.id} />
+        </div>
+      </div>
+
+      {/* Mobile Player Controls */}
+      <div
+        className="
+            flex 
+            md:hidden 
+            col-auto 
+            w-full 
+            justify-end 
+            items-center
+          "
+      >
+        <div
+          onClick={handlePlay}
+          className="
+              h-10
+              w-10
+              flex 
+              items-center 
+              justify-center 
+              rounded-lg 
+              bg-red-500 
+              p-1 
+              cursor-pointer
+            "
+        >
+          <PlayPauseIcon size={30} className="text-black" />
+        </div>
+      </div>
+
+      {/* Desktop Player Controls */}
+      <div
+        className="
+            hidden
+            h-full
+            md:flex 
+            justify-center 
+            items-center 
+            w-full 
+            max-w-[722px] 
+            gap-x-6
+          "
+      >
+        <div
+          onClick={handlePlay}
+          className="
+              flex 
+              items-center 
+              justify-center
+              h-10
+              w-10 
+              rounded-lg 
+              bg-red-500 
+							hover:bg-red-300 
+							transition
+              p-1 
+              cursor-pointer
+            "
+        >
+          <PlayPauseIcon size={30} className="text-black" />
         </div>
       </div>
     </div>
