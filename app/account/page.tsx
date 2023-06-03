9a2af0c1bcdb6196a497541814a31e2a02c0be38
Maruf Bepary <bepary71@gmail.com>
2023-06-03 17:53:15 +0100

    Changed temporary account page to use message component

--- app/account/page.tsx
+++ app/account/page.tsx
@@ -1,20 +1,16 @@
 "use client";
 
+import PageMessage from "@/components/PageMessage";
+
 export const revalidate = 0; // page will not be cached
 
 export default async function NotFound() {
   return (
     <>
-      <div className="mt-2 mb-7 px-6">
-        <div className="flex justify-between items-center">
-          <div className="grid align-items-center gap-4">
-            <h1 className=" text-3xl font-semibold text-red-500">Account</h1>
-            <h2 className="text-white text-xl font-medium">
-              This feature has not been implemented yet.
-            </h2>
-          </div>
-        </div>
-      </div>
+      <PageMessage
+        title="Account"
+        description="This feature has not been implemented yet"
+      />
     </>
   );
 }
