e36cfeb2d338e29b096726e6601d15b93c23c49a
Maruf Bepary <bepary71@gmail.com>
2023-06-03 17:38:07 +0100

    Improved not found page to look nicer

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,22 +1,22 @@
 "use client";
 
+import Box from "@/components/Box";
+
 export const revalidate = 0; // page will not be cached
 
 export default async function NotFound() {
   return (
-    <>
-      <div className="mt-2 mb-7 px-6">
-        <div className="flex justify-between items-center">
-          <div className="grid align-items-center gap-4">
-            <h1 className=" text-3xl font-semibold text-red-500">
-              404: Page Not Found
-            </h1>
-            <h2 className="text-white text-xl font-medium">
-              This page does not exist. Try navigating to a different page.
-            </h2>
-          </div>
+    <Box className="h-full flex items-center justify-center">
+      <div className="flex justify-between items-center">
+        <div className="grid align-items-center gap-4">
+          <h1 className=" text-3xl font-semibold text-red-500">
+            404: Page Not Found
+          </h1>
+          <h2 className="text-white text-xl font-medium">
+            This page does not exist. Try navigating to a different page.
+          </h2>
         </div>
       </div>
-    </>
+    </Box>
   );
 }
