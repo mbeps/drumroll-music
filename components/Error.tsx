4de97092b1b19bd580e7e4e752f636b7348ef88b
Maruf Bepary <bepary71@gmail.com>
2023-06-03 17:36:28 +0100

    Improved error page to look nicer

--- components/Error.tsx
+++ components/Error.tsx
@@ -5,7 +5,16 @@ import Box from "@/components/Box";
 const ErrorMessage = () => {
   return (
     <Box className="h-full flex items-center justify-center">
-      <div className="text-neutral-400">Something went wrong.</div>
+      <div className="flex justify-between items-center">
+        <div className="grid align-items-center gap-4">
+          <h1 className=" text-3xl font-semibold text-red-500">
+            Something went wrong
+          </h1>
+          <h2 className="text-white text-xl font-medium">
+            There has been an unknown error. More info in the Dev console.
+          </h2>
+        </div>
+      </div>
     </Box>
   );
 };
