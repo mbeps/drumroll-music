de08bf2032d600e9e88357eb25f5ab8839c90ab2
Maruf Bepary <bepary71@gmail.com>
2023-06-05 22:50:27 +0100

    Moved sidebar and its children into a Sidebar directory to keep everything organised

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import Sidebar from "@/components/Sidebar";
+import Sidebar from "@/components/Sidebar/Sidebar";
 import "./globals.css";
 import { Figtree } from "next/font/google";
 import SupabaseProvider from "@/providers/SupabaseProvider";
