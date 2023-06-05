1acfda28421e7fe07a2557cd913e1268f791abe1
Maruf Bepary <bepary71@gmail.com>
2023-06-05 18:33:53 +0100

    Added documentation for the providers

--- providers/ModalProvider.tsx
+++ providers/ModalProvider.tsx
@@ -5,6 +5,13 @@ import { ProductWithPrice } from "@/types/types";
 import AuthModal from "@/components/Modals/AuthModal";
 import UploadModal from "@/components/Modals/UploadModal";
 
+/**
+ * Responsible for rendering authentication and upload modals.
+ * It prevents errors in server-side rendering by ensuring that modals are only rendered on the client side (modals can cause hydration errors).
+ *
+ * @param {ModalProviderProps}
+ * @returns (JSX.Element): provider for all modals
+ */
 const ModalProvider: React.FC = () => {
   const [isMounted, setIsMounted] = useState(false);
 
@@ -14,10 +21,11 @@ const ModalProvider: React.FC = () => {
    * Do not render modal if rendering on the server.
    */
   useEffect(() => {
-    setIsMounted(true);
+    setIsMounted(true); // render modals on the client
   }, []);
 
   if (!isMounted) {
+    // Do not render modals on the server
     return null;
   }
 
