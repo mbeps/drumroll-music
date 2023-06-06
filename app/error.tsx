b9f08815b2b5824a9cfbc9f32fee2651cb402deb
Maruf Bepary <bepary71@gmail.com>
2023-06-06 14:01:02 +0100

    Added documentation for the root pages

--- app/error.tsx
+++ app/error.tsx
@@ -2,6 +2,11 @@
 
 import ErrorMessage from "@/components/Error";
 
+/**
+ * Renders when there is an error.
+ *
+ * @returns (React.FC): error page
+ */
 const Error = () => {
   return <ErrorMessage />;
 };
