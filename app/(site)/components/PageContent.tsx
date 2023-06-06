4397f154d22ec7cc6e8698b5642406e2add395c4
Maruf Bepary <bepary71@gmail.com>
2023-06-06 14:06:21 +0100

    Added documentation and comments for the root page of the site

--- app/(site)/components/PageContent.tsx
+++ app/(site)/components/PageContent.tsx
@@ -8,8 +8,17 @@ interface PageContentProps {
   songs: Song[];
 }
 
+/**
+ * Displays a list of songs in a grid.
+ * This is responsive and will display a different number of columns.
+ * If there are no songs, a message will be displayed.
+ * Clicking on a song will play it.
+ *
+ * @param songs (Song[]): list of songs to be displayed
+ * @returns (React.FC): list of songs
+ */
 const PageContent: React.FC<PageContentProps> = ({ songs }) => {
-  const onPlay = useOnPlay(songs);
+  const onPlay = useOnPlay(songs); // allows the user to play a song
 
   if (songs.length === 0) {
     return <div className="mt-4 text-neutral-400">No songs available.</div>;
