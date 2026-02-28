dd6903560acdc40b3953ee24cdda96f48f72dda0
Maruf Bepary <bepary71@gmail.com>
2026-02-28 11:28:10 +0000

    FIX: Songs cards were too large on the search screen

--- app/search/components/SearchContent.tsx
+++ app/search/components/SearchContent.tsx
@@ -1,25 +1,18 @@
 "use client";
 
-import ListSongs from "@/components/LikedSongs";
-import useOnPlay from "@/hooks/useOnPlay";
 import { Song } from "@/types/types";
+import SongsGrid from "@/components/SongsGrid";
 
 interface SearchContentProps {
   songs: Song[];
 }
 
-/**
- * Renders the search results.
- * If there are no search results, a message will be displayed.
- * Otherwise, the list of songs will be displayed.
- *
- * @param songs (Song[]): list of songs (search results
- * @returns (React.FC): list of songs with like buttons
- */
 const SearchContent: React.FC<SearchContentProps> = ({ songs }) => {
-  const onPlay = useOnPlay(songs);
-
-  return <ListSongs songs={songs} message="No songs found" onPlay={onPlay} />;
+  return (
+    <div className="px-6 pb-4">
+      <SongsGrid songs={songs} />
+    </div>
+  );
 };
 
 export default SearchContent;
