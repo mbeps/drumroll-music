a555c91dc0924fda19ffbf54cfc73ae1d9cdf08c
Maruf Bepary <bepary71@gmail.com>
2026-03-06 15:10:13 +0000

    DOCS: Added docs to mobile check hook

--- hooks/use-mobile.ts
+++ hooks/use-mobile.ts
@@ -2,6 +2,26 @@ import * as React from "react"
 
 const MOBILE_BREAKPOINT = 768
 
+/**
+ * Custom hook that determines if the current viewport width is within the defined mobile breakpoint.
+ * 
+ * It sets up a media query listener to track changes in the window size and updates the state
+ * accordingly. This is useful for responsive components that need to change behavior or
+ * rendering based on the device type.
+ * 
+ * @returns `true` if the current viewport width is less than `MOBILE_BREAKPOINT`, otherwise `false`.
+ * 
+ * @example
+ * ```tsx
+ * const isMobile = useIsMobile();
+ * 
+ * return (
+ *   <div>
+ *     {isMobile ? <MobileNavigation /> : <DesktopNavigation />}
+ *   </div>
+ * );
+ * ```
+ */
 export function useIsMobile() {
   const [isMobile, setIsMobile] = React.useState<boolean | undefined>(undefined)
 
